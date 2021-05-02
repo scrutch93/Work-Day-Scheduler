6f9cacff3f6b3e3fef97808d3487c7ecb5c41de9
Steven Crutchfield <stevccru@icloud.com>
2021-05-01 21:41:39 -0400

    added time IDs to be able to target specific rows

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,9 @@
 var displayTime = moment().toString();
 
 document.getElementById("time").innerHTML = displayTime;
+
+
+document.querySelector("card").addEventListener("click", function(){
+    if displayTime > 
+
+}
